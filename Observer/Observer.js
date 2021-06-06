1ec9ea9d873caa0aabeb64b04129b5c1d50433da
PetarParushev <lightone3591@gmail.com>
2021-06-06 16:11:09 +0300

    Add Observer

---
+++ Observer/Observer.js
@@ -0,0 +1,37 @@
+class Subject {
+    constructor() {
+        this.observers = [];
+    }
+    subscribe(func) {
+        this.observers.push(func);
+    }
+    unsubscribe(func) {
+        this.observers = this.observers.filter(f => {
+            if (f != func) {
+                return f;
+            }
+        });
+    }
+    trigger() {
+        this.observers.forEach(func => {
+            func.call();
+        });
+    }
+}
+
+const observer = new Subject();
+
+const observable1 = () => {
+    console.log('Hi from observable 1.');
+}
+
+const observable2 = () => {
+    console.log('Hi from observable 2.');
+}
+
+observer.subscribe(observable1);
+observer.trigger(); // Logs 1
+observer.subscribe(observable2);
+observer.trigger(); // Logs 1 and 2
+observer.unsubscribe(observable1);
+observer.trigger(); // Logs 2K
